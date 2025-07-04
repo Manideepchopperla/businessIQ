fbc27c57de9d79e0745bf03eae7558cc74ea7725
Manideep <chopperlamanideep1029@gmail.com>
2025-07-04 16:09:45 +0530

    added frontend and backend deployed url into .env file

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173',process.env.FRONTEND_URL],
+  credentials: true,
+}
+));
 app.use(express.json());
 
 // Sample SEO headlines for regeneration
